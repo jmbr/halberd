e11dfd39087adc07e10d4ad1cb04ccae261a983a
rwx <jmbr@superadditive.com>
2004-02-15 05:33:11 +0100

    Refactored and documented several important functions.
    Refactored and documented several important functions.
    The examples in the documentation can be automatically checked out by doctest.
    
    darcs-hash:20040215043311-a3a09-5ad5998781afe518a2280aaa9d56f1ff16216caa.gz

--- Halberd/clues/analysis.py
+++ Halberd/clues/analysis.py
@@ -17,12 +17,13 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-__revision__ = '$Id: analysis.py,v 1.1 2004/02/13 01:17:43 rwx Exp $'
+"""Clue analysis and miscellaneous utilities.
 
+XXX Write good documentation (with doctest)
+"""
 
-# ========================
-# Clue analysis functions.
-# ========================
+__revision__ = '$Id: analysis.py,v 1.2 2004/02/15 04:33:11 rwx Exp $'
+
 
 def diff_fields(clues):
     """Study differences between fields.
@@ -33,25 +34,28 @@ def diff_fields(clues):
     @return: Fields which were found to be different among the analyzed clues.
     @rtype: C{list}
     """
+    def pairs(num):
+        for i in xrange(num):
+            for j in xrange(num):
+                if i == j:
+                    continue
+                yield (i, j)
+
     import difflib
 
     scores = {}
 
-    for i in range(len(clues)):
-        for j in range(len(clues)):
-            if i == j:
+    for i, j in pairs(len(clues)):
+        one, other = clues[i].headers, clues[j].headers
+        matcher = difflib.SequenceMatcher(None, one, other)
+
+        for tag, alo, ahi, blo, bhi in matcher.get_opcodes():
+            if tag == 'equal':
                 continue
-
-            one, other = clues[i].headers, clues[j].headers
-            matcher = difflib.SequenceMatcher(None, one, other)
-    
-            diffs = [opcode for opcode in matcher.get_opcodes() \
-                            if opcode[0] != 'equal']
-
-            for tag, alo, ahi, blo, bhi in diffs:
-                for name, value in one[alo:ahi] + other[blo:bhi]:
-                    scores.setdefault(name, 0)
-                    scores[name] += 1
+                
+            for name, value in one[alo:ahi] + other[blo:bhi]:
+                scores.setdefault(name, 0)
+                scores[name] += 1
 
     total = sum(scores.values())
     result = [(count * 100 / total, field) for field, count in scores.items()]
@@ -63,20 +67,40 @@ def diff_fields(clues):
 
 def unzip(seq):
     """Inverse of zip.
+
+    >>> unzip([('a', 1), ('b', 2), ('c', 3)])
+    (('a', 'b', 'c'), (1, 2, 3))
     """
     return tuple(zip(*seq))
 
+def get_digest(clue):
+    """Returns the specified clue's digest.
+    """
+    return clue.info['digest']
+
 def decorate_and_sort(clues):
     """Decorates a list of clues and sorts it by their time diff.
+
+    >>> import Clue
+    >>> a, b, c = Clue.Clue(), Clue.Clue(), Clue.Clue()
+    >>> a.diff, b.diff, c.diff = range(1, 4)
+    >>> clues = undecorate(decorate_and_sort([c, b, a]))
+    >>> (clues[0] == a, clues[1] == b, clues[2] == c)
+    (True, True, True)
     """
     decorated = [(clue.diff, clue) for clue in clues]
     decorated.sort()
     return decorated
 
-def find_clusters(clues, step=3):
+def undecorate(decorated):
+    """Undecorate a list of decorated clues.
+    """
+    return unzip(decorated)[-1]
+
+def clusters(clues, step=3):
     """Finds clusters of clues.
 
-    A cluster is a group of at most 3 clues which only differ in 1 seconds
+    A cluster is a group of at most C{step} clues which only differ in 1 seconds
     between each other.
     """
     def iscluster(clues, num):
@@ -94,6 +118,9 @@ def find_clusters(clues, step=3):
                 return tuple(clues[:num])
         return ()
 
+    # Sort the clues by their time difference.
+    clues = undecorate(decorate_and_sort(clues))
+
     invrange = lambda num: [(num - x) for x in range(num)]
 
     start = 0
@@ -109,12 +136,29 @@ def find_clusters(clues, step=3):
                 start = i
                 break
 
-def merge_cluster(cluster):
-    """Merges a given cluster into one clue.
+def merge(clues):
+    """Merges a sequence of clues into one.
+
+    The first clue from the sequence will be the one aggregating the count values
+    from the rest.
+
+    >>> from Clue import Clue
+    >>> a, b, c = Clue(), Clue(), Clue()
+
+    We are aware each Clue has a starting count of 1
+    >>> sum([x.getCount() for x in [a, b, c]])
+    3
+    >>> a.incCount(5), b.incCount(11), c.incCount(23)
+    (None, None, None)
+    >>> merged = merge((a, b, c))
+    >>> merged.getCount()
+    42
+    >>> merged == a
+    True
     """
-    for clue in cluster[1:]:
-        cluster[0].incCount(clue.getCount())
-    return cluster[0]
+    for clue in clues[1:]:
+        clues[0].incCount(clue.getCount())
+    return clues[0]
 
 def classify(seq, *classifiers):
     """Classify a sequence according to one or several criteria.
@@ -136,41 +180,65 @@ def classify(seq, *classifiers):
 
     return classified
 
-def sections(classified, sets=None):
+def sections(classified, sects=None):
     """Returns sections (and their items) from a nested dict.
     """
-    if sets is None:
-        sets = []
+    if sects is None:
+        sects = []
 
-    # XXX turn this into a generator.
     if isinstance(classified, dict):
         for key in classified.keys():
-            sections(classified[key], sets)
+            sections(classified[key], sects)
     elif isinstance(classified, list):
-        sets.append(classified)
+        sects.append(classified)
 
-    return sets
+    return sects
 
-def get_deltas(diffs):
-    """Generator function which yields the differences between the elements of
-    a list of integers.
+def deltas(diffs):
+    """Returns the differences between the elements of a sequence of integers.
+
+    >>> list(deltas([-1, 0, 1]))
+    [None, 1, 1]
+    >>> list(deltas([1, 1, 3, 2, 3, 4, 6, 2]))
+    [None, 0, 2, -1, 1, 1, 2, -4]
+    >>> list(deltas(range(10)))
+    [None, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     """
     prev = None
     for diff in diffs:
         if prev is None:
             prev = diff
-            yield 0     # Maybe it should return None instead.
+            yield None
         else:
             delta = diff - prev
             prev = diff
             yield delta
 
-def get_slices(clues, indexes):
-    start, end = 0, len(clues)
+def slices(seq, indexes):
+    """Returns slices of a given sequence separated by the specified indexes.
+
+    @param seq: Sequence to split in slices.
+    @type seq: Any (mutable) sequence which provides de __len__ method.
+
+    @return: A sequence of C{slice} objects suitable for splitting the given
+    sequence.
+    @rtype: C{tuple}
+
+    For example. We want to get the slices necessary to split range(20) in
+    sub-sequences of 5 items each:
+    >>> seq, points = range(20), range(5, 20, 5)
+    >>> for sl in slices(seq, points):
+    ...     print seq[sl]
+    [0, 1, 2, 3, 4]
+    [5, 6, 7, 8, 9]
+    [10, 11, 12, 13, 14]
+    [15, 16, 17, 18, 19]
+    """
+    start, end = 0, len(seq)
     for idx in indexes:
-        yield (start, idx)
+        yield slice(start, idx)
         start = idx
-    yield (start, end)
+    yield slice(start, end)
 
 def filter_proxies(clues, maxdelta=3):
     """Detect and merge clues pointing to a proxy cache on the remote end.
@@ -178,9 +246,8 @@ def filter_proxies(clues, maxdelta=3):
     results = []
 
     # Classify clues by remote time and digest.
-    getrtime = lambda c: c._remote
-    getdigest = lambda c: c.info['digest']
-    classified = classify(clues, getrtime, getdigest)
+    get_rtime = lambda c: c._remote
+    classified = classify(clues, get_rtime, get_digest)
 
     subsections = sections(classified)
     for section in subsections:
@@ -189,39 +256,36 @@ def filter_proxies(clues, maxdelta=3):
 
         # We find the indexes of those clues which differ from the rest in
         # more than maxdelta seconds.
-        indexes = [idx for idx, delta in enumerate(get_deltas(diffs)) \
-                       if delta > maxdelta]
+        indexes = [idx for idx, delta in enumerate(deltas(diffs)) \
+                       if delta is not None and abs(delta) > maxdelta]
 
-        for i, j in get_slices(cur_clues, indexes):
-            for clue in cur_clues[i:j]:
+        for sl in slices(cur_clues, indexes):
+            for clue in cur_clues[sl]:
                 clues.remove(clue)
 
-            results.append(merge_cluster(cur_clues[i:j]))
+            results.append(merge(cur_clues[sl]))
 
     return results
 
 def uniq(clues):
     """Return a list of unique clues.
 
-    This is needed when merging clues coming from different sources.
+    This is needed when merging clues coming from different sources. Clues with
+    the same time diff and digest are not discarded, they are merged into one
+    clue with the aggregated number of hits.
     """
     results = []
 
-    getdiff = lambda c: c.diff
-    getdigest = lambda c: c.info['digest']
-    classified = classify(clues, getdigest, getdiff)
+    get_diff = lambda c: c.diff
+    classified = classify(clues, get_digest, get_diff)
 
     for section in sections(classified):
-        results.append(merge_cluster(section))
+        results.append(merge(section))
 
     return results
 
 def analyze(clues):
     """Draw conclusions from the clues obtained during the scanning phase.
-
-    First we group all the clues at hand by their digest, second we sort them
-    by time difference and afterwards we try to find groups of at most 3 clues
-    which actually belong to the same real server.
 
     @param clues: Unprocessed clues obtained during the scanning stage.
     @type clues: C{list}
@@ -231,22 +295,19 @@ def analyze(clues):
     """
     results = []
 
-    # XXX maybe the following two functions should be written as generators.
-    clues = filter_proxies(uniq(clues))
+    clues = uniq(clues)
 
-    # We discriminate the clues by their digests.
-    getdigest = lambda c: c.info['digest']
-    cluesby = classify(clues, getdigest)
-    for digest in cluesby.keys():
-        # Sort by time difference.
-        group = unzip(decorate_and_sort(cluesby[digest]))[1]
+    clues = filter_proxies(clues)
 
-        for cluster in find_clusters(group):
-            results.append(merge_cluster(cluster))
+    cluesbydigest = classify(clues, get_digest)
+
+    for key in cluesbydigest.keys():
+        for cluster in clusters(cluesbydigest[key]):
+            results.append(merge(cluster))
 
     return results
 
-
+
 # =============================
 # Misc. clue-related utilities.
 # =============================
@@ -282,6 +343,14 @@ def load_clues(filename):
     cluefp.close()
 
     return clues
+
+
+def _test():
+    import doctest, analysis
+    return doctest.testmod(analysis)
+
+if __name__ == '__main__':
+    _test()
 
 
 # vim: ts=4 sw=4 et
