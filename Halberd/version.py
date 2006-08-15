bef21fc9eda3819f7834a8c98affa599770d025f
Juan M. Bello Rivas <jmbr+halberd@superadditive.com>
2006-08-16 01:18:56 +0200

    Added Halberd/version.py to the repository.
    darcs-hash:20060815231856-d00bf-11c92362f7d7d4495a0f4eada4886cbac9ee2421.gz

---
+++ Halberd/version.py
@@ -0,0 +1,15 @@
+##
+##  ./Halberd/version.py -- Version Information for halberd (syntax: Python)
+##  [automatically generated and maintained by GNU shtool]
+##
+
+class version:
+    v_hex       = 0x00120C
+    v_short     = "0.1.12"
+    v_long      = "0.1.12 (29-Jul-2006)"
+    v_tex       = "This is halberd, Version 0.1.12 (29-Jul-2006)"
+    v_gnu       = "halberd 0.1.12 (29-Jul-2006)"
+    v_web       = "halberd/0.1.12"
+    v_sccs      = "@(#)halberd 0.1.12 (29-Jul-2006)"
+    v_rcs       = "$Id: halberd 0.1.12 (29-Jul-2006) $"
+
