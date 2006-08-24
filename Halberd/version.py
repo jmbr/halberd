1b2283f915d62469daed9495d4000d4b4b15af4b
Juan M. Bello Rivas <jmbr+halberd@superadditive.com>
2006-08-24 18:38:24 +0200

    Bumped version number
    darcs-hash:20060824163824-d00bf-b8bc49d63fba95c3eecfc719302cfa0a4cb916b8.gz

--- Halberd/version.py
+++ Halberd/version.py
@@ -4,12 +4,12 @@
 ##
 
 class version:
-    v_hex       = 0x002200
-    v_short     = "0.2.0"
-    v_long      = "0.2.0 (17-Aug-2006)"
-    v_tex       = "This is halberd, Version 0.2.0 (17-Aug-2006)"
-    v_gnu       = "halberd 0.2.0 (17-Aug-2006)"
-    v_web       = "halberd/0.2.0"
-    v_sccs      = "@(#)halberd 0.2.0 (17-Aug-2006)"
-    v_rcs       = "$Id: halberd 0.2.0 (17-Aug-2006) $"
+    v_hex       = 0x002201
+    v_short     = "0.2.1"
+    v_long      = "0.2.1 (24-Aug-2006)"
+    v_tex       = "This is halberd, Version 0.2.1 (24-Aug-2006)"
+    v_gnu       = "halberd 0.2.1 (24-Aug-2006)"
+    v_web       = "halberd/0.2.1"
+    v_sccs      = "@(#)halberd 0.2.1 (24-Aug-2006)"
+    v_rcs       = "$Id: halberd 0.2.1 (24-Aug-2006) $"
 
